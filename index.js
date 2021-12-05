ee522d673a26788218a068050d36b1c1c73758d7
Gustavo Porto <gustavoporto@ya.ru>
2021-12-05 16:38:27 +0000

    test: answering good increases interval

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
 const AGAIN = -3;
 const HARD = -1;
 const GOOD = 1;
-const DEFAULT_INTERVALS = [1, 2, 3, 8, 17];
-const DEFAULT_SCORE = [AGAIN, HARD, GOOD];
+const INTERVALS = [1, 2, 3, 8, 17];
+const SCORES = [AGAIN, HARD, GOOD];
 const DAY_IN_MS = 24 * 60 * 60 * 1000;
 
 export default class Card {
 	constructor(
-		intervals = DEFAULT_INTERVALS,
-		scoreToIntervalChange = DEFAULT_SCORE
+		progress = 0,
+		dueDate = Math.round(new Date().getTime() / DAY_IN_MS)
 	) {
-		this.intervals = intervals;
-		this.scoreToIntervalChange = scoreToIntervalChange;
+		this.progress = progress;
+		this.dueDate = dueDate;
+		this.intervals = INTERVALS;
+		this.scoreToProgressChange = SCORES;
 	}
 
-	get maxInterval() {
-		return this.intervals[this.intervals.length];
+	get maxProgress() {
+		return this.intervals.length;
 	}
 
-	calculate(score, { interval, dueDate }) {
-		const now = Math.round(new Date().getTime() / DAY_IN_MS);
+	calculate(score, { progress, dueDate }) {
 		const correct = score === GOOD;
-		const nextIntervalIndex =
-			this.intervals.findIndex((i) => i === interval) + 1;
-		const newInterval = this.intervals[nextIntervalIndex];
 		const newDueDate =
-			correct && interval < this.maxInterval
-				? now + newInterval 
-				: now + 1;
+			correct && progress < this.maxProgress
+				? dueDate + this.intervals[progress]
+				: dueDate + 1;
 
 		return {
 			dueDate: newDueDate,
-			interval: newInterval ?? 0,
-		};
-	}
-
-	getInitialRecord() {
-		const now = Math.round(new Date().getTime() / DAY_IN_MS);
-
-		return {
-			interval: 0,
-			dueDate: now,
+			progress: progress++ ?? 0,
 		};
 	}
 }
--- index.test.js
+++ index.test.js
@@ -1,15 +1,24 @@
 import Card from './index';
 
+const DAY_IN_MS = 24 * 60 * 60 * 1000;
+const lastDueDate = Math.round(new Date('2000-01-01').getTime() / DAY_IN_MS);
+
 describe(Card.name, () => {
-	test('should review in one day', () => {
-		const card = new Card();
-		const record = card.getInitialRecord();
-		const updatedRecord = card.calculate(1, record);
+	test.each([
+		[{ progress: 0, dueDate: lastDueDate }, 1],
+		[{ progress: 1, dueDate: lastDueDate }, 2],
+		[{ progress: 2, dueDate: lastDueDate }, 3],
+		[{ progress: 3, dueDate: lastDueDate }, 8],
+		[{ progress: 4, dueDate: lastDueDate }, 17],
+	])('GOOD - record: %O, nextInterval: %i', (record, nextInterval) => {
+		const score = 1;
+		const card = new Card(record.progress, record.dueDate);
+		const updatedRecord = card.calculate(score, record);
 
 		expect(updatedRecord).toEqual(
 			expect.objectContaining({
-				dueDate: expect.any(Number),
-				interval: 1,
+				dueDate: record.dueDate + nextInterval,
+				progress: record.progress,
 			})
 		);
 	});
